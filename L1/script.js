79d1214fafa64bb50d2966a220bb8618fc929c49
MuhammadHasaan546-eng <muhammadhasaanm546@gmail.com>
2026-07-20 15:35:07 +0500

    timeLine to from

--- L1/script.js
+++ L1/script.js
@@ -1,36 +1,58 @@
-// gsap.to("#box1", {
-//   x: 200,
+// // gsap.to("#box1", {
+// //   x: 200,
+// //   duration: 1,
+// //   delay: 1,
+// // });
+
+// // gsap.to("#box2", {
+// //   x: 200,
+// //   duration: 1,
+// //   delay: 2,
+// // });
+
+// // gsap.to("#box3", {
+// //   x: 200,
+// //   duration: 1,
+// //   delay: 5,
+// // });
+
+// const tl = gsap.timeline();
+
+// tl.to("#box1", {
+//   x: 800,
 //   duration: 1,
+//   rotate: 360,
 //   delay: 1,
 // });
-
-// gsap.to("#box2", {
-//   x: 200,
+// tl.to("#box2", {
+//   x: 400,
 //   duration: 1,
-//   delay: 2,
+//   rotate: 360,
 // });
-
-// gsap.to("#box3", {
+// tl.to("#box3", {
 //   x: 200,
 //   duration: 1,
-//   delay: 5,
+//   rotate: 360,
 // });
 
 const tl = gsap.timeline();
 
-tl.to("#box1", {
-  x: 800,
-  duration: 1,
-  rotate: 360,
-  delay: 1,
+tl.from("h2", {
+  y: -30,
+  opacity: 0,
+  duration: 2,
+  delay: 0.5,
 });
-tl.to("#box2", {
-  x: 400,
+
+tl.from(".part2 h4", {
+  y: -20,
+  opacity: 0,
   duration: 1,
-  rotate: 360,
+  stagger: 0.5,
 });
-tl.to("#box3", {
-  x: 200,
-  duration: 1,
-  rotate: 360,
+
+tl.from("h1", {
+  opacity: 0,
+  duration: 2,
+  scale: 0.3,
 });
