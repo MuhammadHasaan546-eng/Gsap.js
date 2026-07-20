a8aaf24aa0ae9bcf8a6364f34e137a8547482e47
MuhammadHasaan546-eng <muhammadhasaanm546@gmail.com>
2026-07-20 14:56:30 +0500

    stagger

--- L1/script.js
+++ L1/script.js
@@ -2,6 +2,10 @@ gsap.to("#box1", {
   x: 1000,
   duration: 2,
   delay: 1,
+  rotate: 360,
+  backgroundColor: "blue",
+  borderRadius: "50%",
+  scale: 1.5,
 });
 
 gsap.from("#box2", {
@@ -9,4 +13,13 @@ gsap.from("#box2", {
   //   y: 500,
   duration: 2,
   delay: 1,
+});
+
+gsap.from("h1", {
+  y: 30,
+  duration: 1,
+  delay: 1,
+  color: "blue",
+  opacity: 0,
+  stagger: -1,
 });
