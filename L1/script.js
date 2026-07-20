ab7c4ee26ec225c9367633ecbde51d2dc6dba74c
MuhammadHasaan546-eng <muhammadhasaanm546@gmail.com>
2026-07-20 15:21:36 +0500

    timeLine

--- L1/script.js
+++ L1/script.js
@@ -1,25 +1,36 @@
-gsap.to("#box1", {
-  x: 1000,
-  duration: 2,
-  delay: 1,
-  rotate: 360,
-  backgroundColor: "blue",
-  borderRadius: "50%",
-  scale: 1.5,
-});
+// gsap.to("#box1", {
+//   x: 200,
+//   duration: 1,
+//   delay: 1,
+// });
 
-gsap.from("#box2", {
-  x: 1000,
-  //   y: 500,
-  duration: 2,
-  delay: 1,
-});
+// gsap.to("#box2", {
+//   x: 200,
+//   duration: 1,
+//   delay: 2,
+// });
 
-gsap.from("h1", {
-  y: 30,
+// gsap.to("#box3", {
+//   x: 200,
+//   duration: 1,
+//   delay: 5,
+// });
+
+const tl = gsap.timeline();
+
+tl.to("#box1", {
+  x: 800,
   duration: 1,
+  rotate: 360,
   delay: 1,
-  color: "blue",
-  opacity: 0,
-  stagger: -1,
+});
+tl.to("#box2", {
+  x: 400,
+  duration: 1,
+  rotate: 360,
+});
+tl.to("#box3", {
+  x: 200,
+  duration: 1,
+  rotate: 360,
 });
